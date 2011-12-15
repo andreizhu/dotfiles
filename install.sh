8dd36672518c1b5ea6ca8addd45bd6d2b5d72f07
andreizhu <andrei.zhu@gmail.com>
2011-12-15 14:45:26 +0800

    install bash script

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ ln -s $DIR/pentadactyl/ap.js ~/.pentadactyl/plugins/
 #awesome
 ln -s $DIR/awesome/rc.lua ~/.config/awesome/
 #vim
-ln -s $DIR/vim/vimrc ~/.vim/.vimrc
+ln -s $DIR/vim/vimrc ~/.vimrc
 ln -s $DIR/vim/zenburn.vim ~/.vim/colors/
 ln -s $DIR/vim/taglist.vim ~/.vim/plugin/
